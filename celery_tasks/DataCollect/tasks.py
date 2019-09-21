5a1d0ef4be35c60616e07d462ba033cfed92a6d9
chenzhiyuan <imczy@vip.qq.com>
2019-09-21 22:26:19 +0800

    add celery for DataCollect

--- celery_tasks/DataCollect/tasks.py
+++ celery_tasks/DataCollect/tasks.py
@@ -6,7 +6,7 @@ from DataCollect import Init_StockALL_Sp
 
 logger = get_task_logger('get_dayline')
 
-@celery_app.task(name='')
+@celery_app.task(name='QFpj.DataCollect.Init_StockALL_Sp.get_dayline')
 def get_dayline():
     update_stock = Init_StockALL_Sp.get_dayline()
     print('All Stocks in List %s Are Up-to-date' % (update_stock))
